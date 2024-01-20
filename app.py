89e4d8c3f77f448e6238859264cf1e9e98f48922
Set Paing <setpaing20022002@gmail.com>
2024-01-20 03:20:22 -0800

    flask

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
-from flask import Flask, render_template, request
-from transcribe import download_audio, audio_to_text
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '3f500e5c4689bb1221d1bbc545931d898f30c1d9c2a9ad6a'
+
+transcript_results = [{'title': 'FUCK YOU', 'content': 'NNN'}]
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
-@app.route('/process_url', methods=['POST'])
-def process_url():
-    youtube_url = request.form['youtube_url']
-    output_path = "path/to/downloaded/audio.mp3"
-    # download_audio(youtube_url, output_path)
-    text_result = audio_to_text(output_path)
-    return render_template('result.html', text_result=text_result)
+@app.route('/results/')
+def results():
+    return render_template('transcripts.html', messages=transcript_results)
+
+@app.route('/yt_transcribe/', methods=('GET', 'POST'))
+def yt_transcribe():
+    if request.method == 'POST':
+        title = request.form['yt_url']
+
+        if not title:
+            flash('YouTube Link is required!')
+        else:
+            transcript_results.append({'title': title, 'content': 'NNN'})
+            return redirect(url_for('results'))
+    return render_template('yt_transcribe.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
