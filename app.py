f6658a59857219ac3474d15de73c001f26deadc5
Set Paing <setpaing20022002@gmail.com>
2024-01-20 05:17:18 -0800

    erorrrsss

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 from transcribe import audio_to_text
 from getVideos import yt_to_mp3
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3f500e5c4689bb1221d1bbc545931d898f30c1d9c2a9ad6a'
 
-transcript_results = [{'title': 'FUCK YOU', 'content': 'NNN'}]
+transcript_results = [{'title': 'Extra Long Video Title',
+                       'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}]
 
 @app.route('/')
 def home():
@@ -20,15 +22,17 @@ def yt_transcribe():
     if request.method == 'POST':
         title = request.form['yt_url']
 
-        # file_name = yt_to_mp3(title)
-        # file_name
-        # tran_rs = (audio_to_text(file_name))
+        file_name = yt_to_mp3(title)
+        
+        tran_rs = (audio_to_text(file_name))
 
         if not title:
             flash('YouTube Link is required!')
         else:
-            transcript_results.append({'title': title, 'content': 'tran_rs'})
+            transcript_results.append({'title': title, 'content': tran_rs})
+            os.remove(file_name)
             return redirect(url_for('results'))
+        
     return render_template('yt_transcribe.html')
 
 if __name__ == '__main__':
