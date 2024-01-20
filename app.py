b4c83270bb70859c670467893c9866bb28720146
JJ Yu <jiajunyu123@gmail.com>
2024-01-20 13:46:36 -0800

    added download button

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
-from transcribe import audio_to_text
+from flask import Flask, render_template, request, url_for, flash, redirect, send_file
+from transcribe import audio_to_text, download_notes
 from getVideos import yt_to_mp3
 import os
 
@@ -13,15 +13,20 @@ transcript_results = []
 def home():
     return render_template('home.html')
 
+name = ""
+tran_rs = ""
+
 @app.route('/yt_transcribe/', methods=('GET', 'POST'))
 def yt_transcribe():
+    global name, tran_rs
+
     if request.method == 'POST':
         if 'yt_url' in request.form:
             title = request.form['yt_url']
 
             file_name, name = yt_to_mp3(title)
             if file_name != 0:
-                tran_rs = (audio_to_text(file_name))
+                tran_rs = audio_to_text(file_name)
                 transcript_results.append({'title': name, 'content': tran_rs})
                 os.remove(file_name)
             else:
@@ -30,9 +35,23 @@ def yt_transcribe():
             if 'return_code' in request.form:
                 transcript_results.clear()
             elif 'download_code' in request.form:
-                print("FUCKKKKKKKKKKKKKKKKKKKKKKKKKKKKk")
-                 
+                download_file()
+
     return render_template('yt_transcribe.html', transcript_results=transcript_results)
+
+@app.route('/download')
+def download_file():
+    # title = "Sample Title"
+    # body = "This is the body of the text file."
+    global name, tran_rs
+    content = f"{name}\n\n{tran_rs}"
+
+    with open(name + '.txt', 'w') as file:
+        file.write(content)
+
+    return send_file(name + '.txt', as_attachment=True)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
